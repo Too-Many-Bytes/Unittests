abce48f47ece45feaaccbbf6ad61a0ca9234b280
Too-Many-Bytes <82663828+Too-Many-Bytes@users.noreply.github.com>
2021-12-18 16:17:43 +0300

    Add files via upload

---
+++ functions.py
@@ -0,0 +1,62 @@
+def read_file(path: str):
+    cases = []
+    with open(path, 'r', encoding='utf-8') as file:
+        for item in file:
+            parsed_line = item.strip().split()
+            for i in range(len(parsed_line)):
+                try:
+                    parsed_line[i] = int(parsed_line[i])
+                except ValueError:
+                    continue
+            cases.append(parsed_line)
+    return cases
+
+
+def find_min(massive: list):
+    try:
+        current_min = massive[0]
+        for i in massive:
+            if i < current_min:
+                current_min = i
+        return current_min
+    except OverflowError:
+        print('Ошибка переполнения!')
+    except TypeError:
+        print('Некорректый ввод!')
+
+
+def find_max(massive: list):
+    try:
+        current_max = massive[0]
+        for i in massive:
+            if i > current_max:
+                current_max = i
+        return current_max
+    except OverflowError:
+        print('Ошибка переполнения!')
+    except TypeError:
+        print('Некорректый ввод!')
+
+
+def find_sum(mass: list):
+    try:
+        total_sum = 0
+        for i in mass:
+            total_sum += i
+        return total_sum
+    except OverflowError:
+        print('Ошибка переполнения!')
+    except TypeError:
+        print('Некорректый ввод!')
+
+
+def find_product(mass: list):
+    try:
+        product = 1
+        for i in mass:
+            product *= i
+        return product
+    except OverflowError:
+        print('Ошибка переполнения')
+    except TypeError:
+        print('Некорректый ввод!')
